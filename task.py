661785cbddcf57a931601a4d05d6cdedebcd7799
zagumionnov <zagumionnov6@gmail.com>
2021-12-01 01:14:13 +0200

    Extract data from PDF. Compare the values

--- task.py
+++ task.py
@@ -1,6 +1,9 @@
+import os
+import re
 import time
 
 from RPA.Browser.Selenium import Selenium
+from RPA.PDF import PDF
 
 from utils import WorkWithExcel
 
@@ -12,7 +15,13 @@ class ParseAgencies:
         self.agency_info = {}
         self.excel = WorkWithExcel()
         self.all_agencies = []
+        self.uii = []
         self.uii_url = []
+        self.investment_title = []
+        self.browser_lib.set_download_directory(os.path.join(os.getcwd(), 'output'))
+        self.pdf = PDF()
+        self.name_of_inv = []
+        self.unique_inv_ident = []
 
     def open_the_website(self, url):
         self.browser_lib.open_available_browser(url)
@@ -23,8 +32,7 @@ class ParseAgencies:
 
     def get_agencies(self, xpath):
         self.browser_lib.wait_until_element_is_visible(
-            "//div[@id='agency-tiles-widget']//div[@class='col-sm-4 text-center noUnderline']", 15
-        )
+            "//div[@id='agency-tiles-widget']//div[@class='col-sm-4 text-center noUnderline']", 15)
         self.all_agencies = self.browser_lib.find_elements(xpath)
         list_of_agency = [agency.text.split("\n") for agency in self.all_agencies]
         name_of_agency = [value[0] for value in list_of_agency]
@@ -56,15 +64,42 @@ class ParseAgencies:
             value = row.find_elements_by_tag_name('td')
             try:
                 self.uii_url.append(value[0].find_element_by_tag_name('a').get_attribute('href'))
+                self.investment_title.append(value[2].text)
+                self.uii.append(value[0].text)
             except:
-                print('Element not found!')
+                pass
             values = [val.text for val in value]
             data.append(values)
 
         self.excel.append_row_to_sheet(data, path, sheet)
 
     def download_pdf(self):
-        pass
+        for url in self.uii_url:
+            self.browser_lib.go_to(url)
+            self.browser_lib.wait_until_page_contains_element('//div[@id="business-case-pdf"]')
+            self.browser_lib.find_element('//div[@id="business-case-pdf"]').click()
+            self.browser_lib.wait_until_element_is_visible("//div[@id='business-case-pdf']//span", 15)
+            self.browser_lib.wait_until_element_is_not_visible("//div[@id='business-case-pdf']//span", 15)
+        time.sleep(3)
+
+    def get_pdf_data(self):
+        for pdf_name in self.uii:
+            text = self.pdf.get_text_from_pdf(f'output/{pdf_name}.pdf')
+            self.name_of_inv.append(re.search(
+                r'Name of this Investment:(.*)2\.', text[1]).group(1).strip())
+            self.unique_inv_ident.append(re.search(
+                r'Unique Investment Identifier \(UII\):(.*)Section B', text[1]).group(1).strip())
+
+    def compare_pdf(self):
+        for i in range(len(self.investment_title)):
+            if self.name_of_inv[i] == self.investment_title[i]:
+                print(f'PDF name: {self.name_of_inv[i]} comfirmed!')
+            else:
+                print(f'PDF name: {self.name_of_inv[i]} does not match')
+            if self.unique_inv_ident[i] == self.uii[i]:
+                print(f'PDF UII:{self.unique_inv_ident[i]} comfirmed!')
+            else:
+                print(f'PDF UII:{self.unique_inv_ident[i]} does not match')
 
 
 if __name__ == "__main__":
@@ -79,6 +114,9 @@ if __name__ == "__main__":
         a.excel.append_row_to_sheet(a.agency_info, 'output/agencies.xlsx', 'Agencies')
         a.get_agency_page(24)
         a.get_individual_invest('output/agencies.xlsx', 'Individual Investments')
+        a.download_pdf()
+        a.get_pdf_data()
+        a.compare_pdf()
 
     finally:
         a.browser_lib.close_all_browsers()
